92558c35b53faee4c07fdfcac8bfa981ff101fd1
muatik <muatik@gmail.com>
2015-09-05 22:41:36 +0300

    prepares vagrant environment

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install apache2
+sudo apt-get install mongodb-server
+sudo apt-get install python-pip
+sudo apt-get install supervisor
+
+sudo pip install flask
+sudo pip install schema
+sudo pip install colorlog
+sudo pip install pillow
+sudo pip install pymongo
